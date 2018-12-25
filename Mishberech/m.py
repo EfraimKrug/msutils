565ab005b264ced97d139c87e6726a8d75734fdb
efraim krug <efraimmkrug@gmail.com>
2018-12-25 12:37:34 -0500

    mishberech routines - complete

--- Mishberech/m.py
+++ Mishberech/m.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-word = "שלום"
-print(word.encode('UTF-8'))
+word = u'שלום'
+wordE = 'az'
+wordR = word[::-1]
+print(wordR)
+print(ord(word[0]))
+print(wordE)
+print(ord(wordE[1]))
